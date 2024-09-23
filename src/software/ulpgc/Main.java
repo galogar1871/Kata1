9aeda1b72b931e3b5b8f9936e86bda90836dbede
Gabriel López Garcia <gabriellopezgarcia12@gmail.com>
2024-09-23 12:20:39 +0100

    Second Version

--- src/software/ulpgc/Main.java
+++ src/software/ulpgc/Main.java
@@ -4,6 +4,6 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Person("Juan", LocalDate.of(2005,4,4)));
+        System.out.println(new Person("Pepe", LocalDate.of(2005,4,4)));
     }
 }
