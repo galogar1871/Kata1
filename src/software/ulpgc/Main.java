4d389f5fa3e90889548a53ed3cbb9b14fd118486
Gabriel López Garcia <gabriellopezgarcia12@gmail.com>
2024-09-29 17:48:54 +0100

    First Method

---
+++ src/software/ulpgc/Main.java
@@ -0,0 +1,9 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("pepe", LocalDate.of(2002,12,1)));
+    }
+}
---
+++ src/software/ulpgc/Person.java
@@ -0,0 +1,38 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int getAge(){
+        return toAge(LocalDate.now().toEpochDay() - birthday.toEpochDay());
+    }
+
+    private int toAge(long days) {
+        return (int) (days/365);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", age=" + getAge() +
+                '}';
+    }
+}
